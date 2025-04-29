cf4c52cb4f995e82be38aaec65bc6678b67458b7
satishtiwari811 <satishtiwari811@gmail.com>
2025-04-29 18:56:06 +0530

    Added files

---
+++ README.md
@@ -0,0 +1,3 @@
+# sp-user-authentication-service-
+The sp-user-authentication-service is a backend microservice responsible for handling user authentication and authorization processes.
+project work is in progress.
---
+++ src/test/java/sp_user_authentication_service/SpUserAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sp_user_authentication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpUserAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
